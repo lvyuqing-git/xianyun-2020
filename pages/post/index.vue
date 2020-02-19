2a692e38eb5441eddb89f2da353056903f0b52c7
一打砖头拍不倒 <lyq15360562781@163.com>
2020-02-20 01:25:00 +0800

    添加分页组件

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,21 +1,61 @@
 <template>
-  <div class="container">
-    <div class="left">
-      <IndexAside></IndexAside>
+  <div>
+    <div class="container">
+      <div class="clearfix">
+        <div class="left">
+          <IndexAside></IndexAside>
+        </div>
+        <div class="right">
+          <IndexSearch @strategyData='setStrategyData'></IndexSearch>
+          <!-- <IndexStrategy></IndexStrategy> -->
+        </div>
+      </div>
     </div>
-    <div class="right">
-      <IndexSearch></IndexSearch>
-    </div>
+   <div class="paging">
+        <el-pagination @size-change="handleSizeChange"
+                   @current-change="handleCurrentChange"
+                   :current-page="currentPage"
+                   :page-sizes="[3, 5, 10, 15]"
+                   :page-size="100"
+                   layout="total, sizes, prev, pager, next, jumper"
+                   :total="StrategyData.total">
+    </el-pagination>
+   </div>
   </div>
 </template>
 
 <script>
 import IndexAside from '@/components/post/indexAside'
 import IndexSearch from '@/components/post/indexSearch'
+// import IndexStrategy from '@/components/post/indexStrategy'
 export default {
+    data () {
+        return {
+            //接受搜索框的数据
+            StrategyData : {},
+            // 分页框当前页数
+            currentPage : 0
+        }
+    },
   components: {
     IndexAside,
-    IndexSearch,
+    IndexSearch
+    // IndexStrategy
+  },
+  methods: {
+      //搜索框传递过来的搜索结果
+      setStrategyData(data){
+          this.StrategyData = data
+          console.log(data);
+      },
+      //分页框选择条数时触发
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      //分页框选择页数时触发
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
   }
 }
 </script>
@@ -24,7 +64,6 @@ export default {
 .container {
   width: 1000px;
   margin: 20px auto;
-  height: 800px;
   display: flex;
   .left {
     width: 260px;
@@ -32,10 +71,13 @@ export default {
   }
   .right {
     float: left;
-    flex: 1;
+    width: 700px;
     margin-left: 40px;
     box-sizing: border-box;
-    
   }
+}
+.paging{
+    text-align: center;
+    padding-bottom: 15px;
 }
 </style>
--- store/post.js
+++ store/post.js
@@ -1,17 +1,30 @@
 export const state = () => {
   return {
-    a: 123
+    strategyList: {}
   };
+};
+
+export const mutations = {
+  setStrategy(state, data) {
+    state.strategyList = data;
+  }
 };
 
 export const actions = {
   //攻略首页搜索接口
-  citySearch(store,params) {
+  citySearch(store, params) {
+    let bl = false;
+    if (!params.city) {
+      bl = true;
+    }
+    if (bl) {
+      delete params.city
+    }
     return this.$axios({
       url: "/posts",
       params
     }).then(res => {
-     return res
+      return res;
     });
   }
 };
