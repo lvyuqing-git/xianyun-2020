82a6963130a760b5a8c0e5323ac3b44e2deaa0bf
caifu23 <1224758467@qq.com>
2020-02-19 17:43:23 +0800

    cf-攻略详情页- 08 添加攻略评论输入框，照片墙

--- components/post/detailcomments.vue
+++ components/post/detailcomments.vue
@@ -7,9 +7,38 @@
           <i class="iconfont iconpinglun"></i>
           <span>评论(100)</span>
         </el-col>
-        <el-col :span="3" @click.native=" $message.warning({message: '暂不支持分享', type: 'warning', offset: 30})" class="cursor">
+        <el-col
+          :span="3"
+          @click.native=" $message.warning({message: '暂不支持分享', type: 'warning', offset: 30})"
+          class="cursor"
+        >
           <i class="iconfont iconfenxiang"></i>
           <span>分享</span>
+        </el-col>
+      </el-row>
+    </div>
+    <p class="comments-title">评论</p>
+    <!-- 评论输入区  -->
+    <div class="comments-input">
+      <!-- 输入框-文本域 -->
+      <el-input type="textarea" :rows="2" placeholder="说点什么吧..." v-model="textarea" resize="none"></el-input>
+      <!-- 照片墙+按钮 -->
+      <el-row type="flex" justify="space-between">
+        <el-col class="comments-upload">
+          <el-upload
+            action="https://jsonplaceholder.typicode.com/posts/"
+            list-type="picture-card"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove"
+          >
+            <i class="el-icon-plus"></i>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt />
+          </el-dialog>
+        </el-col>
+        <el-col :span="2">
+            <el-button type="primary" size="mini">提交</el-button>
         </el-col>
       </el-row>
     </div>
@@ -24,12 +53,22 @@ export default {
       default: {}
     }
   },
-  data () {
-      return {
-          
-      }
+  data() {
+    return {
+      textarea: "", //评论内容输入
+      dialogImageUrl: "",
+      dialogVisible: false
+    };
   },
-  methods: {},
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  },
   mounted() {}
 };
 </script>
@@ -40,7 +79,7 @@ export default {
     padding: 30px 0;
     text-align: center;
     .cursor {
-        cursor: pointer;
+      cursor: pointer;
     }
     i {
       display: block;
@@ -52,6 +91,22 @@ export default {
       font-size: 14px;
       color: #999;
     }
+  }
+  .comments-title {
+    margin: 15px 0 8px;
+    font-size: 20px;
+  }
+  .comments-input {
+      /deep/textarea {
+          margin-bottom: 10px;
+      }
+  }
+  .comments-upload {
+      /deep/.el-upload, /deep/.el-upload-list__item {
+          width: 100px;
+          height: 100px;
+          line-height: 100px;
+      }
   }
 }
 </style>
