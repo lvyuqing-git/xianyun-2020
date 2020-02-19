973dc3d8e68c219243eb1998d99d4d2331d71b97
caifu23 <1224758467@qq.com>
2020-02-19 13:03:43 +0800

    cf-攻略详情页- 01 根据文章id获取文章详情

---
+++ pages/post/detail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="post-detail">攻略详情</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 获取攻略详情
+    getDetail() {
+      if (this.$route.query.id) {
+        // 获取攻略id
+        let id = Number(this.$route.query.id);
+        // console.log(id);
+        // 请求文章详情
+        this.$axios({
+            url: '/posts',
+            params: {
+                id
+            }
+        }).then(( {data: {data}} ) => {
+            console.log(data)
+            if(data.length) {
+              
+            }
+        })
+
+      }
+    }
+  },
+  mounted() {
+    // 获取攻略
+    this.getDetail();
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
