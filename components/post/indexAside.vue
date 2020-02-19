927001185172f7decda0bb228d5a29b979205444
一打砖头拍不倒 <lyq15360562781@163.com>
2020-02-19 18:30:35 +0800

    lyq-攻略主页-01 左侧边栏完成

--- components/post/indexAside.vue
+++ components/post/indexAside.vue
@@ -4,12 +4,20 @@
       <div class="asidetop">
         <ul>
           <li v-for="(item,index) in cityList"
-              :key="index">
-            <p>{{item.type}}<i class="el-icon-arrow-right"></i></p>
-            <div class="asideconseal">
-                <!-- <div v-for="(item,index) in item.children" :key="index">
-                    <p v-if="number === index">{{item.city}}</p>
-                </div> -->
+              :key="index"
+              @mouseover="alternumber(index)"
+              class="fatherli">
+            <span>{{item.type}}</span><i class="el-icon-arrow-right icon"></i>
+            <div class="asideconseal"
+                 v-show="index == number">
+              <ul>
+                <li v-for="(item2,index2) in item.children"
+                    :key="index2"
+                    class="sonli">
+                  <p class="childrens"><span class="index">{{index2+1}}</span> <span class="area">{{item2.city}}</span><i class="conten">{{item2.desc}}</i></p>
+                </li>
+              </ul>
+              <div class="line"></div>
             </div>
           </li>
         </ul>
@@ -17,8 +25,8 @@
       </div>
       <div class="asidebottom">
         <p>推荐城市</p>
-        <img src="pic_sea.jpeg"
-             alt="">
+        <!-- <img src="pic_sea.jpeg"
+             alt=""> -->
       </div>
     </div>
   </div>
@@ -29,7 +37,7 @@ export default {
   data() {
     return {
       cityList: [],
-      number : 0
+      number: 0
     }
   },
   mounted() {
@@ -38,6 +46,11 @@ export default {
     }).then(res => {
       this.cityList = res.data.data
     })
+  },
+  methods: {
+    alternumber(index) {
+      this.number = index
+    }
   }
 }
 </script>
@@ -51,31 +64,58 @@ export default {
         display: block;
       }
     }
-    > ul {
-      li {
-        height: 40px;
-        font-size: 14px;
+
+    .fatherli {
+      height: 40px;
+      font-size: 14px;
+      line-height: 40px;
+      border: 1px solid #dddddd;
+      padding: 0 15px;
+      margin-bottom: -1px;
+      .icon {
+        float: right;
         line-height: 40px;
-        border: 1px solid #dddddd;
-        padding: 0 15px;
-        margin-bottom: -1px;
-        p {
-          i {
-            float: right;
-            line-height: 40px;
-            font-size: 20px;
-            color: #a7a7a7;
-          }
+        font-size: 20px;
+        color: #a7a7a7;
+      }
+      &:hover {
+        border-right: none;
+        color: #ffc14f;
+        i {
+          color: #ffc14f;
+        }
+        .conten{
+            color: #9a9a9a;
         }
       }
     }
     .asideconseal {
       position: absolute;
-      border: 1px solid #dddddd;
       top: 0px;
-      left: 260px;
+      left: 239px;
       display: none;
-    
+      border: 1px solid #dddddd;
+      border-left: none;
+      .sonli {
+        height: 40px;
+        line-height: 40px;
+        padding-left: 20px;
+        width: 400px;
+        .index {
+          font-size: 20px;
+          vertical-align:middle;
+          font-style: italic;
+        }
+        .area{
+            margin: 0 15px 0
+        }
+      }
+      .line{
+          position: absolute;
+          top: 164px;
+          border-left: 1px solid #dddddd;
+          height: 37px;
+      }
     }
   }
   .asidebottom {
