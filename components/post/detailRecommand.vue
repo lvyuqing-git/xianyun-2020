4ec00e6e73524dcfdb1533d888d6d73c8cb2aa76
caifu23 <1224758467@qq.com>
2020-02-23 13:39:12 +0800

    cf-攻略详情页- 29 当点击推荐攻略时，与左侧文章详情同步更新数据（阅读量-请求文章时阅读量会加1）；修改右侧攻略推荐的样式

--- components/post/detailRecommand.vue
+++ components/post/detailRecommand.vue
@@ -3,19 +3,22 @@
     <!-- 攻略推荐 -->
     <h3>相关攻略</h3>
     <!-- 攻略简要信息 -->
-    <nuxt-link class="detail-brief" :to="'/post/detail?id='+ item.id" v-for="(item, index) in recommandList" :key="index">
+    <nuxt-link
+      class="detail-brief"
+      :to="'/post/detail?id='+ item.id"
+      v-for="(item, index) in recommandList"
+      :key="index"
+    >
       <el-row type="flex">
         <el-col :span="14">
           <div class="title">{{item.title}}</div>
           <div class="info">
-            <span> {{publishTime(item.created_at)}} </span>
+            <span>{{publishTime(item.created_at)}}</span>
             <span>阅读 {{ item.watch || 0 }}</span>
           </div>
         </el-col>
-        <el-col :span="10">
-          <!-- 图片资源缺少，此处占位 -->
-          <div v-html="item.content" v-show="false"></div>
-          <img :src="item.images[0]" alt="" v-show="item.images.length" style="width: 100%;">
+        <el-col :span="10" class="recommand-img">
+          <img :src="item.images[0]" alt v-show="item.images.length" />
         </el-col>
       </el-row>
     </nuxt-link>
@@ -24,7 +27,7 @@
 
 <script>
 // 引入moment插件
-import moment from 'moment'
+import moment from "moment";
 export default {
   data() {
     return {
@@ -34,12 +37,11 @@ export default {
   mounted() {
     // 获取攻略推荐
     this.getRecommand();
-    
   },
   methods: {
     // 获取攻略推荐
     getRecommand() {
-      //   console.log(this.$route.query.id);
+      // console.log(this.$route.query.id);
       if (this.$route.query.id) {
         // 获取文章id
         let id = Number(this.$route.query.id);
@@ -61,12 +63,35 @@ export default {
     publishTime(time) {
       return moment(new Date(time)).format("YYYY-MM-DD HH:mm");
     }
+  },
+  computed: {
+    // 监听id变化, --- 弃用，改用 watch监听 $route
+    test () {
+      // 延时获取 攻略推荐信息，
+      // 以便跟左边文章详情  “阅读量” 达到同步
+      setTimeout(() => {
+        // 获取攻略推荐
+        this.getRecommand();
+      }, 100);
+      return this.$route.query.id
+    }
+  },
+  watch: {
+    // 监听route（针对 query.id）变化
+    '$route.query.id'(val, oldVal) {
+      // 延时获取 攻略推荐信息，
+      // 以便跟左边文章详情  “阅读量” 达到同步
+      setTimeout(() => {
+        this.getRecommand()
+      }, 100);
+    }
   }
 };
 </script>
 
 <style lang="less" scoped>
 .detail-recommand {
+  margin-right: -15px;
   h3 {
     border-bottom: 1px solid #ddd;
     padding: 0 0 10px;
@@ -77,7 +102,12 @@ export default {
     border-bottom: 1px solid #ddd;
     padding: 15px 0 20px;
     .title {
-      padding: 5px 0 30px;
+      position: relative;
+      margin: 5px 0 30px;
+      line-height: 1.4em;
+      /* 2 times the line-height to show 2 lines */
+      height: 2.8em;
+      overflow: hidden;
     }
     .info {
       font-size: 11px;
@@ -85,6 +115,16 @@ export default {
       span {
         margin-right: 8px;
       }
+    }
+  }
+  .recommand-img {
+    margin-left: 8px;
+    padding-top: 10px;
+    width: 100px;
+    height: 80px;
+    img {
+      width: 100%;
+      height: 100%;
     }
   }
 }
