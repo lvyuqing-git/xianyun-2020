efa4bf171a927b7e0846d4f6bf52b2c931210eec
一打砖头拍不倒 <lyq15360562781@163.com>
2020-02-23 20:22:07 +0800

    lh-02-酒店全局图渲染完成

--- pages/hotel/detail.vue
+++ pages/hotel/detail.vue
@@ -9,48 +9,63 @@
     </div>
     <!-- 酒店介绍 -->
     <div class="introduce">
-        <h2 class="hotailName">{{detailInfo.name}}</h2>
-        <p class="pinyin">{{detailInfo.alias}}</p>
-        <p class="pinyin">{{detailInfo.address}}</p>
+      <h2 class="hotailName">{{detailInfo.name}}</h2>
+      <p class="pinyin">{{detailInfo.alias}}</p>
+      <p class="pinyin "><i class="el-icon-location"></i>{{detailInfo.address}}</p>
+    </div>
+    <!-- 酒店图 -->
+    <div>
+      <Hotelimg></Hotelimg>
     </div>
   </div>
 </template>
 
 <script>
+import Hotelimg from '@/components/hotel/hotelImg'
 export default {
-  data () {
-      return {
-         detailInfo : {} 
-      }
+  data() {
+    return {
+      detailInfo: {}
+    }
   },
-  mounted () {
+  mounted() {
     //  let id =  this.$route.query.id
 
-      this.$axios({
-          url : `/hotels?id=185` 
-      }).then((res)=>{
-          console.log(res.data.data);
-          this.detailInfo = res.data.data[0]
-      })
+    this.$axios({
+      url: `/hotels?id=185`
+    }).then(res => {
+      console.log(res.data.data)
+      this.detailInfo = res.data.data[0]
+    })
+  },
+  components: {
+    Hotelimg
   }
 }
 </script>
 
 <style lang='less' scoped>
-.detail{
-    width: 1000px;
-    margin: 0 auto;
+.detail {
+  width: 1000px;
+  margin: 0 auto;
+  margin-top: 20px;
+  height: 800px;
+  .introduce {
     margin-top: 20px;
-    height: 800px;
-    .introduce{
-        margin-top: 20px;
-        .hotailName{
-            font-weight: normal;
-        }
-        .pinyin{
-                color: #666;
-    font-size: 14px;
-        }
+    .hotailName {
+      font-weight: normal;
     }
+    .pinyin {
+      color: #666;
+      font-size: 14px;
+      line-height: 16px;
+      i{
+          font-size: 18px;
+      }
+    }
+  }
+  .img{
+      margin-top: 40px;
+  }
 }
 </style>
